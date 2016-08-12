63f60f1a9c8c698fd3229d2290692e2b0ac8fe72
Tom Holoien <tholoien@gmail.com>
2016-08-12 16:02:17 -0700

    Started setup.py #7

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="xdgmm",
+    version=xdgmm.__version__,
+    author="Tom Holoien",
+    author_email="tholoien@gmail.com",
+    url="https://github.com/tholoien/XDGMM",
+    packages=["xdgmm"],
+    description="A wrapper class for the scikit-learn BaseEstimator class that implements both the astroML and Bovy et al. (2011) XDGMM methods.",
+    long_description=open("README.md").read(),
+    package_data={"": ["README.md", "LICENSE"]},
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    install_requires=["numpy", "astroML", "scikit-learn", "scipy"],
+)
