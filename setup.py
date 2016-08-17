8cfc2f297fb6af4d7e6417af8654b9b33909857b
Tom Holoien <tholoien@gmail.com>
2016-08-17 11:18:54 -0700

    Updated version in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import xdgmm
 
 setup(
     name="xdgmm",
-    version='0.0.1',
+    version='0.0.2',
     author="Tom Holoien",
     author_email="tholoien@gmail.com",
     url="https://github.com/tholoien/XDGMM",
